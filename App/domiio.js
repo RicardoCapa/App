6357432a4f880eea4a22d04e901bebfef02fdaf4
RicardoCapa <33547541+RicardoCapa@users.noreply.github.com>
2018-08-06 00:46:50 -0500

    Add files via upload

---
+++ App/domiio.js
@@ -0,0 +1,9 @@
+var exp = require('express');
+var app=exp();
+var path = require('path');
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/PaginaButtom.html'));
+});
+
+app.listen(3000);
